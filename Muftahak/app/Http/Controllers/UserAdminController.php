879b2920abb4eac752f77d4a2b2195460e828743
joroh <jorohahmad@gmail.com>
2025-12-16 00:13:03 +0300

    logout

--- Muftahak/app/Http/Controllers/UserAdminController.php
+++ Muftahak/app/Http/Controllers/UserAdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rented;
+use App\Models\User;
 use App\Models\UserAdmin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserAdminController extends Controller
@@ -39,4 +42,67 @@ class UserAdminController extends Controller
     {
         return UserAdmin::all();
     }
+
+    //login
+    function login(Request $request)
+    {
+        $request->validate([
+            'phoneNumber' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('phoneNumber', $request->phoneNumber)->first();
+        if ($user) {
+            if (! $user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'message' => 'user login not successful because you password not valid'
+                ], 401);
+            }
+
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'user login successfully',
+                'user' => $user,
+                'Token' => $token
+            ], 200);
+        }
+
+        $user = Rented::where('phoneNumber', $request->phoneNumber)->first();
+        if ($user) {
+            if (! $user || !Hash::check($request->password, $user->password))
+                return response()->json([
+                    'message' => 'rented login not successful because you password not valid'
+                ], 401);
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'rented login successfully',
+                'user' => $user,
+                'Token' => $token
+            ], 200);
+        }
+        return response()->json([
+            'message'=>'not found'
+        ],404);
+    }
+
+    //logout
+    function logout(Request $request)
+   {
+      $rented=$request->user('renteds-api');
+      if($rented){
+        $rented->currentAccessToken()->delete();
+                     return response()->json([
+         'message' => 'rented logout successfully',
+      ], 200);
+      }
+       $user=Auth::user();
+    if($user){
+      $request->user()->currentAccessToken()->delete();
+                     return response()->json([
+         'message' => 'user logout successfully',
+      ], 200);
+      }
+   }
 }
